5356e53063c7d915cc168b0d637f17c525c86603
Carolina Leguizamon <63667103+ispeakfishlanguage@users.noreply.github.com>
2024-09-23 17:28:57 +0200

    Add displaying of historical throw results

--- js/app.js
+++ js/app.js
@@ -5,12 +5,17 @@ const gameResult = document.getElementById('game-result');
 const win = document.getElementById('win');
 const lose = document.getElementById('lose');
 const tie = document.getElementById('tie');
+const playerHistory = document.getElementById('playerHistory');
+const computerHistory = document.getElementById('computerHistory');
 
 let playerResult;
 let computerResult;
 let wins = 0;
 let loses = 0;
 let ties = 0;
+
+let playerHistoryThrows = [];
+let computerHistoryThrows = [];
 
 function throwDice () {
   return Math.floor(Math.random() * 6) + 1;
@@ -20,22 +25,34 @@ function updateScore () {
   if (playerResult === computerResult) {
     gameResult.innerHTML = "It's a tie!";
     ties++;
+  } else if (playerResult < computerResult) {
+    gameResult.innerHTML = "Computer wins!";
+    loses++;
   } else {
-    gameResult.innerHTML = playerResult < computerResult ? "Computer wins!": "You win!";
-    playerResult < computerResult ? loses++ : wins++;
+    gameResult.innerHTML = "You win!";
+    wins++;
   }
   win.innerHTML = "Won: " + wins;
-  lose.innerHTML = "Lose: " + loses;
-  tie.innerHTML = "Won: " + ties;
+  lose.innerHTML = "Lost: " + loses;
+  tie.innerHTML = "Tie: " + ties;
 }
 
+function updateHistory () {
+  playerHistoryThrows.push(playerResult);
+  computerHistoryThrows.push(computerResult);
+  playerHistory.innerHTML = "Player: " + playerHistoryThrows;
+  computerHistory.innerHTML = "Computer: " + computerHistoryThrows;
+}
 
-dice.addEventListener('click', () =>{
+function playARound () {
   playerResult = throwDice();
   playerScore.innerHTML = playerResult;
   computerResult = throwDice();
   computerScore.innerHTML = computerResult;
   updateScore();
-});
+  updateHistory();
+}
+
+dice.addEventListener('click', playARound);
 
 
