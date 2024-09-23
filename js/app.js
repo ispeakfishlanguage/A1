97e8cbe299bd6078641cf4aa1c14d9ffe3cdf048
Carolina Leguizamon <63667103+ispeakfishlanguage@users.noreply.github.com>
2024-09-23 16:53:25 +0200

    define variables and constant variables

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const playerScore = document.getElementById('score-player');
+const computerScore = document.getElementById('score-computer');
+const dice = document.getElementById('dice');
+const gameResult = document.getElementById('game-result');
+const win = document.getElementById('win');
+const lose = document.getElementById('lose');
+const tie = document.getElementById('tie');
+
+let playerResult;
+let computerResult;
+let wins = 0;
+let loses = 0;
+let ties = 0;
+
