99c041109b2636587c703f95864227b8fd9f0c0a
Carolina Leguizamon <63667103+ispeakfishlanguage@users.noreply.github.com>
2024-10-02 20:46:40 +0200

    Added cookies

--- js/app.js
+++ js/app.js
@@ -14,14 +14,30 @@ let wins = 0;
 let loses = 0;
 let ties = 0;
 
-let playerHistoryThrows = [];
-let computerHistoryThrows = [];
+// Initialize history arrays
+let playerScoreHistory = [];
+let computerScoreHistory = [];
 
-function throwDice () {
+// Get previous results from cookies
+let previousPlayerScore = getCookie("playerScoreHistory");
+let previousComputerScore = getCookie("computerScoreHistory");
+
+// Check if previous scores exist and update history if so
+if (previousPlayerScore) {
+  playerScoreHistory = previousPlayerScore.split(','); // Split the string back into an array
+  playerHistory.innerHTML = "Player: " + playerScoreHistory.join(', ');
+}
+
+if (previousComputerScore) {
+  computerScoreHistory = previousComputerScore.split(','); // Split the string back into an array
+  computerHistory.innerHTML = "Computer: " + computerScoreHistory.join(', ');
+}
+
+function throwDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-function updateScore () {
+function updateScore() {
   if (playerResult === computerResult) {
     gameResult.innerHTML = "It's a tie!";
     ties++;
@@ -37,14 +53,17 @@ function updateScore () {
   tie.innerHTML = "Tie: " + ties;
 }
 
-function updateHistory () {
-  playerHistoryThrows.push(playerResult);
-  computerHistoryThrows.push(computerResult);
-  playerHistory.innerHTML = "Player: " + playerHistoryThrows;
-  computerHistory.innerHTML = "Computer: " + computerHistoryThrows;
+function updateHistory() {
+  playerScoreHistory.push(playerResult);
+  computerScoreHistory.push(computerResult);
+  playerHistory.innerHTML = "Player: " + playerScoreHistory.join(', ');
+  computerHistory.innerHTML = "Computer: " + computerScoreHistory.join(', ');
+
+  // Save the updated history to cookies
+  saveAsCookie();
 }
 
-function playARound () {
+function playARound() {
   playerResult = throwDice();
   playerScore.innerHTML = playerResult;
   computerResult = throwDice();
@@ -55,4 +74,22 @@ function playARound () {
 
 dice.addEventListener('click', playARound);
 
+// Function to save score history as cookies
+function saveAsCookie() {
+  document.cookie = "playerScoreHistory=" + playerScoreHistory + ";expires=Thu, 18 Dec 2025 12:00:00 UTC";
+  document.cookie = "computerScoreHistory=" + computerScoreHistory + ";expires=Thu, 18 Dec 2025 12:00:00 UTC";
+}
 
+// Function to get cookie by name
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i].trim();
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
